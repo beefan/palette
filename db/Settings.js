fb8c314cf6130349516ce01dc477e92a24adec74
Brandon Fannin <bfannin13@gmail.com>
2020-09-13 01:51:07 -0400

    save user settings complete

--- db/Settings.js
+++ db/Settings.js
@@ -17,14 +17,29 @@ async function getUserSettings(req, res) {
 }
 
 async function saveUserSettings(req, res) {
-    const userId = parseInt(req.params.userId)
-    // do something with the request body to get the settings
-
+    if (req.user == null) {
+        res.status(403).send({msg: 'You need to be logged in for that.'});
+        return;
+    }
+    const pf = req.body.prompt_frequency_hrs;
+    const af = req.body.analysis_frequency_hrs;
+    const cl = req.body.check_limit_hrs;
+    const userid = req.user[0].id;
     try {
-        const sql = 'SELECT * FROM tag' // to be changed
-        const {rows} = await db.query(sql);
-        res.status(200).send({ rows });
+        await db.query('START TRANSACTION;');
+        const sql = 'SELECT id FROM settings WHERE userid=$1';
+        const {rows} = await db.query(sql, [userid]);
+        if (rows[0] != null) {
+            const sqll = 'DELETE FROM settings WHERE userid=$1';
+            await db.query(sqll, [userid]);
+        } 
+        const sqlll = 'INSERT INTO settings (userId, prompt_frequency_hrs, analysis_frequency_hrs, check_limit_hrs) VALUES ($1, $2, $3, $4)';
+        await db.query(sqlll, [userid, pf, af, cl]);
+        await db.query('COMMIT;');
+        res.status(200).send({ msg: 'Settings successfully saved.' });
     } catch (err) {
+        await db.query('ROLLBACK;')
+        console.error(err);
         return res.send(err);
     }
 }
---
+++ db/sql/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (username, email, password) 
+VALUES ('bfannin', 'bfannin13@gmail.com', '$2b$05$JKO.9B.e2D.VLuy16eVvN.gI4Qug7r7gBWbDYalYA.EhZEIN1Cw22');
+
+delete from users where id=3;
+
+INSERT INTO settings (userId, prompt_frequency_hrs, analysis_frequency_hrs, check_limit_hrs)
+VALUES (4, 4, 4, 4);
+
+select * from settings where userid=4;
+
+delete from settings where userid=4;
+
+update settings (prompt_frequency_hrs, analysis_frequency_hrs, check_limit_hrs)
+VALUES (4, 4, 4) where userid=4;
